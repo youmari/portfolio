74b6d4b33aa990cf64121915454030778a44676d
youmari <youmari300@gmail.com>
2021-11-08 22:07:02 +0100

    Fix issue of disappearing nav in desktop menu

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const hambIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.nav-links-container');
 const close = document.querySelector('.exit-icon');
 const links = document.querySelectorAll('.nav-link');
+const mobileScreen = window.matchMedia('(max-width: 1029px)');
 
 hambIcon.addEventListener('click', () => {
   menu.style.display = 'flex';
@@ -13,6 +14,7 @@ close.addEventListener('click', () => {
 
 links.forEach((link) => {
   link.addEventListener('click', () => {
-    menu.style.display = 'none';
+    if (mobileScreen.matches)
+      menu.style.display = 'none';
   });
 });
