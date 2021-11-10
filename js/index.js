fb7d3aa5e3df976c9cb29c5b1750666df4f82974
youmari <youmari300@gmail.com>
2021-11-10 22:45:50 +0100

    Change name of img

--- js/index.js
+++ js/index.js
@@ -3,21 +3,25 @@ const menu = document.querySelector('.nav-links-container');
 const close = document.querySelector('.exit-icon');
 const links = document.querySelectorAll('.nav-link');
 const mobileScreen = window.matchMedia('(max-width: 1029px)');
-const projectsData = [{
-  name: 'Covid19 web app',
-  img: '../images/covid-img.jpeg',
-  description: 'a web app that gives you information about the covid 19 around the world',
-  liveLink: 'https://youmari.github.io/Covid-19/',
-  projectRepo: 'https://github.com/youmari/Covid-19',
-  technologies: ['html', 'css', 'javascript'],
-}, {
-  name: 'Face Detection web app',
-  img: '../images/face-img.PNG',
-  description: 'web app that will recognize any face in the given image',
-  liveLink: 'https://youmari.github.io/Face-Detection/',
-  projectRepo: 'https://github.com/youmari/Face-Detection',
-  technologies: ['html', 'css', 'javascript', 'react'],
-}];
+const projectsData = [
+  {
+    name: "Covid19 web app",
+    img: "../images/covid-img.jpeg",
+    description:
+      "a web app that gives you information about the covid 19 around the world",
+    liveLink: "https://youmari.github.io/Covid-19/",
+    projectRepo: "https://github.com/youmari/Covid-19",
+    technologies: ["html", "css", "javascript"],
+  },
+  {
+    name: "Face Detection web app",
+    img: "../images/face-img.png",
+    description: "web app that will recognize any face in the given image",
+    liveLink: "https://youmari.github.io/Face-Detection/",
+    projectRepo: "https://github.com/youmari/Face-Detection",
+    technologies: ["html", "css", "javascript", "react"],
+  },
+];
 
 projectsData.forEach((element, i) => {
   const projectsContainer = document.querySelector('.projects-container');
