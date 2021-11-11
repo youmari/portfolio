cd46be1655276f409b0aa2bc262ea3f8547d59cf
youmari <youmari300@gmail.com>
2021-11-11 08:52:09 +0100

    Fix email validation

--- js/validation.js
+++ js/validation.js
@@ -1,16 +1,17 @@
-const upperCheck = /[A-Z]/g;
 const email = document.getElementById('email');
 const errorMsg = document.querySelector('.error-msg');
 const form = document.querySelector('.contact-form');
 
 form.addEventListener('submit', (e) => {
+  e.preventDefault();
   const msg = [];
-  if (upperCheck.test(email.value)) {
+  if (email.value.match(/[A-Z]/g) !== null) {
     msg.push('uppercase error');
     errorMsg.style.opacity = 1;
+  } else if (msg.length > 0) {
+    e.preventDefault()
   }
-
-  if (msg.length > 0) {
-    e.preventDefault();
+  else {
+    form.submit();
   }
 });
